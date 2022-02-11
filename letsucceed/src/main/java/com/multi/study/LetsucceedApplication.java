ae99cf39bca8bd84f43eb3e7c73960ce054a30e8
[hyovvely] <[kkd94793@naver.com]>
2022-02-11 23:00:01 +0900

    시작

---
+++ letsucceed/src/main/java/com/multi/study/LetsucceedApplication.java
@@ -0,0 +1,13 @@
+package com.multi.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetsucceedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetsucceedApplication.class, args);
+	}
+
+}
---
+++ letsucceed/src/test/java/com/multi/study/LetsucceedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multi.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetsucceedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
